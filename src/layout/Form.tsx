fa50b691328c72c9a66bdd614f41aef4107d88c3
Pawel Kosiec <pawel@capact.io>
2021-12-22 17:33:40 +0100

    Option 2: Apply CSS styles with not that nice selectors

--- src/layout/Form.tsx
+++ src/layout/Form.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { FormProps, withTheme } from "@rjsf/core";
 
-import ObjectFieldTemplate from "./form/ObjectFieldTemplate";
-import TitleField from "./form/TitleField";
-
 import "./Form.css";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Theme } = require("@rjsf/antd");
-Theme.ObjectFieldTemplate = ObjectFieldTemplate;
 
 const JSONSchemaForm = withTheme(Theme);
 
 const formClassName = "json-schema-form";
-
-const customFields = {
-  TitleField: TitleField,
-};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Form(props: FormProps<any>) {
@@ -28,7 +20,6 @@ function Form(props: FormProps<any>) {
   const extendedProps = {
     ...props,
     className: classNames.join(" "),
-    fields: customFields,
   };
 
   return <JSONSchemaForm {...extendedProps} />;
