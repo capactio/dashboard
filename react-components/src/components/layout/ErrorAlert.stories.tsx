ebd682f47a10c55330dc751f1a61c709aa24e574
Pawel Kosiec <pawel@capact.io>
2022-02-09 15:56:58 +0100

    Add Readme files

--- react-components/src/components/layout/ErrorAlert.stories.tsx
+++ react-components/src/components/layout/ErrorAlert.stories.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import ErrorAlert from "./ErrorAlert";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   component: ErrorAlert,
 } as ComponentMeta<typeof ErrorAlert>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof ErrorAlert> = (args) => (
   <ErrorAlert {...args} />
 );
 
 export const StringMessage = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 StringMessage.args = {
   errorMessage: "Test",
 };
